3f0317cddbda749b5cdb1316c2e1f20438befe6a
abeni-al7 <abenezeralebachew3@gmail.com>
2025-04-05 13:02:30 +0300

    Clone starter code

--- app/src/main/java/com/example/unscramble/ui/theme/Shape.kt
+++ app/src/main/java/com/example/unscramble/ui/theme/Shape.kt
@@ -16,19 +16,12 @@
 
 package com.example.unscramble.ui.theme
 
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    headlineMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        fontSize = 28.sp,
-        lineHeight = 36.sp,
-        letterSpacing = 0.5.sp
-    )
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(10.dp),
+    large = RoundedCornerShape(16.dp)
 )
